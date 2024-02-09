32116c81df59265a4d92b133eda8a2c513d250e4
Maestro1982 <kris.tote@telenet.be>
2024-02-09 10:23:51 +0100

    setup nextjs project with additional configurations

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className='h-full w-full flex justify-center items-center'>
+      <h1 className='text-5xl text-white'>Liveblocks Figma Clone</h1>
+    </div>
+  );
+}
